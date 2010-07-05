0be781f3b1803f2abf476b82448099fbc19194a1
garamonfok <serra.francois@gmail.com>
2010-07-05 16:37:15 +0200

    buf....

--- .ete2/codeml/utils.py
+++ .ete2/codeml/utils.py
@@ -14,7 +14,7 @@ def mkdir_p(path):
     '''
     try:
         os.makedirs(path)
-    except OSError as exc: # Python >2.5
+    except OSError, (exc): # Python >2.5
         if exc.errno == errno.EEXIST:
             pass
         else: raise
