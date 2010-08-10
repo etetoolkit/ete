be5d0af9b7e6019acfcecc47bc26380dc04dc724
Jaime Huerta-Cepas <jhcepas@gmail.com>
2010-08-10 12:40:00 +0200

    solved problem with default layuout in multi tree visualization. Added external WebApp handler

--- webplugin/webplugin.py
+++ webplugin/webplugin.py
@@ -10,7 +10,6 @@
 # /var/www/example.html
 # /var/www/jquery-1.4.2.min.js
 #
-#
 # My apache config para el sitio "default":
 #
 # <VirtualHost *:80>
@@ -53,22 +52,22 @@
 # file. This is the whole idea!! enjoy, this a very draft version.
 # 
 
-
 import os
 import time
 import hashlib
-import urlparse
+import cgi
 
-from ete2 import Tree, PhyloTree
+from ete2 import Tree, PhyloTree, __VERSION__
 from ete2.treeview import drawer
-__VERSION__ = "etedev"
 CONFIG = {}
 
 class WebTreeApplication(object):
     def __init__(self):
         self.TreeConstructor = Tree
-        self.actions = {"node": [], "style": [], "face": []}
+        self.actions = {"node": [], "layout": [], "face": []}
         self._layout = None
+        self._treeid2layout = {}
+        self._external_app_handler = None
 
     def register_action(self, name, target, handler, refresh):
         if target not in self.actions: 
@@ -77,15 +76,16 @@ class WebTreeApplication(object):
 
     def set_tree_loader(self, TreeConstructor):
         self._tree = TreeConstructor
-    def set_latout_fn(self, layout_fn):
+    def set_layout_fn(self, layout_fn):
         self._layout = layout_fn
 
     def _get_html_map(self, img_map, treeid):
+
         html_map = '<MAP NAME="imgmap_%s">' %(treeid)
         if img_map["nodes"]:
             for x1, y1, x2, y2, nodeid, text in img_map["nodes"]:
-                html_map += """ <AREA SHAPE="rect" COORDS="%s,%s,%s,%s" onClick='show_context_menu("%s", "node", "%s");' href='javascript:void(%s);'>""" %\
-                    (x1, y1, x2, y2, treeid, nodeid, treeid)
+                html_map += """ <AREA SHAPE="rect" COORDS="%s,%s,%s,%s" onClick='show_context_menu("%s", "node", "%s");' href='javascript:void(0);'>""" %\
+                    (x1, y1, x2, y2, treeid, nodeid)
         if img_map["faces"]:
             for x1, y1, x2, y2, nodeid, text in img_map["faces"]:
                 html_map += """ <AREA SHAPE="rect" COORDS="%s,%s,%s,%s" onClick='show_context_menu("%s", "face", "%s", "%s");' href='javascript:void(0);'>""" %\
@@ -113,37 +113,32 @@ class WebTreeApplication(object):
                 nid = arguments[0]
                 node = get_node_by_id(t, nid)
                 handler(node)
+            elif atype == "layout":
+                self._treeid2layout[treeid] = handler
 
-            elif atype == "style":
-                self.set_layout(handler)
-
-        img_map = render_tree(t, img_path, self._layout)
+        layout_fn = self._treeid2layout.get(treeid, self._layout)
+        img_map = render_tree(t, img_path, layout_fn)
         html_map = self._get_html_map(img_map, treeid)
-        ete_publi = '</br><a href="http://ete.cgenomics.org" style="font-size:8pt;" target="_blank" > %s </a>' %(__VERSION__)
+        ete_publi = '</br><a href="http://ete.cgenomics.org" style="font-size:7pt;" target="_blank" > %s </a>' %(__VERSION__)
         open(tree_path, "w").write(t.write(features=[]))
-        return html_map+"""<img src="%s" USEMAP=#imgmap_%s>""" %(img_url, treeid) + ete_publi + tree_path + treeid
+        return html_map+"""<img src="%s" USEMAP="#imgmap_%s" onclick='javascript:show_context_menu("%s", "layout", "void", "void");' >""" %(img_url, treeid, treeid) + ete_publi
 
     # WSGI web application 
     def __call__(self, environ, start_response):
         """ This function is executed when the application is called
         by the WSGI apache module. It is, therefore, in charge of
         answering web requests."""
-        environ['wsgi.multithread']  = False
-
         path = environ['PATH_INFO'].split("/")
         start_response('202 OK', [('content-type', 'text/plain')])
         if environ['REQUEST_METHOD'].upper() == 'GET' and  environ['QUERY_STRING']:
-            queries = urlparse.parse_qs(environ['QUERY_STRING'])
+            queries = cgi.parse_qs(environ['QUERY_STRING'])
         elif environ['REQUEST_METHOD'].upper() == 'POST' and environ['wsgi.input']:
-            queries = urlparse.parse_qs(environ['wsgi.input'].read())
+            queries = cgi.parse_qs(environ['wsgi.input'].read())
         else:
             queries = {}
 
         method = path[1]
         #return  '\n'.join(map(str, environ.items())) + str(queries) + '\t\n'.join(environ['wsgi.input'])
-        action_name = queries.get("action", None)
-        action_target = queries.get("target", None)
-        nodeid = queries.get("nodeid", None)
         if method == "draw":
             if "tree" in queries and "treeid" in queries:
                 treeid = str(queries["treeid"][0])
@@ -152,15 +147,14 @@ class WebTreeApplication(object):
 
             else:
                 return "No tree found"
-
         elif method == "get_menu": 
             atype = str(queries["atype"][0])
             nodeid = str(queries["nid"][0])
             treeid = str(queries["treeid"][0])
             html = ''
             for aindex, (action, handler, refresh) in enumerate(self.actions.get(atype, [])):
-                html += """ <a  href='javascript:void(0)' onClick='run_action("%s", "%s", "%s", "%s");'> %s </a></br> """ %\
-                    (treeid, atype, nodeid, aindex, action)
+                html += """ <a  href='javascript:void(["%s", "%s", "%s", "%s"])' onClick='run_action("%s", "%s", "%s", "%s");'> %s </a><br> """ %\
+                    (treeid, atype, nodeid, aindex, treeid, atype, nodeid, aindex, action)
             return html+treeid
 
         elif method == "action":
@@ -171,13 +165,14 @@ class WebTreeApplication(object):
             name, handler, refresh = self.actions[atype][aindex]
             if atype in set(["node","face"]):
                 return self._get_tree(treeid=treeid, pre_drawing_action=[atype, handler, [nid]])
-            elif atype == "style":
-                return self._get_tree(treeid, pre_drawing_action=[atype, handler, []])
+            elif atype == "layout":
+                return self._get_tree(treeid=treeid, pre_drawing_action=[atype, handler, []])
             return "Bad guy"
                 
+        elif self._external_app_handler:
+            return self._external_app_handler(environ, start_response, queries)
         else:
             return  '\n'.join(map(str, environ.items())) + str(queries) + '\t\n'.join(environ['wsgi.input'])
-
 
 ### Get info about leave >>> here can be any function of leave ID with HTML return
 def get_node_by_id(t, nid):
@@ -238,20 +233,13 @@ def render_tree(t, img_path, layout=None):
 
     return img_map
 
-
-
-
-
-
-
-
-
-
-
 # USER PART. You can modify this part
 application = WebTreeApplication()
+#CONFIG["temp_dir"] = "/home/services/web/jaime.phylomedb.org/tmp/"
+#CONFIG["temp_url"] = "http://jaime.phylomedb.org/tmp/"
 CONFIG["temp_dir"] = "/var/www/tmp/"
 CONFIG["temp_url"] = "http://localhost/tmp/"
+
 CONFIG["DISPLAY"] = ":0"
 
 def my_collapse(node):
@@ -262,19 +250,34 @@ def my_tree_loader(tree):
     t = PhyloTree(tree)
     return t
 
+def set_as_root(node):
+    node.get_tree_root().set_outgroup(node)
+
 def my_layout(node):
     if hasattr(node, "bsize"):
         node.img_style["size"]= int(node.bsize)
         node.img_style["fgcolor"]= "#ff0000"
     else:
         node.img_style["size"]= 20
+        node.img_style["fgcolor"]= "#cccccc"
+
+def ly2(node):
+    if hasattr(node, "bsize"):
+        node.img_style["size"]= int(node.bsize)
+        node.img_style["fgcolor"]= "#00ff00"
+    else:
+        node.img_style["size"]= 20
 
 
 application.set_tree_loader(my_tree_loader)
-application.set_latout_fn(my_layout)
+#application.set_layout_fn(my_layout)
 
 # [name, applies to, handler, refresh picture ]
 # node/faces -> load current tree, perform action on node and redraw tree
 application.register_action("collapse node", "node", my_collapse, True)
+application.register_action("set as root", "node", set_as_root, True)
 application.register_action("what a face!", "face", my_collapse, True)
+application.register_action("style 1", "layout", ly2, True)
+application.register_action("style 2", "layout", my_layout, True)
+application.register_action("style 3", "layout", None, True)
 
