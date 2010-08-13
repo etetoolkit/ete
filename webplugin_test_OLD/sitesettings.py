16f4d515586a67ca5fd896066623c2aadc271b87
Jaime Huerta-Cepas <jhcepas@gmail.com>
2010-08-13 13:25:40 +0200

    removed old stuff

--- webplugin_test_OLD/sitesettings.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-
-SCRIPT_FOLDER = '/home/ivan/ete2-webplugin/webplugin_test'
-TEMP_FOLDER = '/home/ivan/ete2-webplugin/webplugin_test/temp' # should have 777 permissions
-HOST_NAME   = 'http://ete2/'
