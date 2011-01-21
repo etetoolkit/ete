50b82d69f8e55d1ba7c4f915cf433246defb5a40
Jaime Huerta-Cepas <jhcepas@gmail.com>
2011-01-21 18:25:31 +0100

    basic tester

---
+++ test_draw.py
@@ -0,0 +1,18 @@
+import sys
+
+from ete_dev import Tree
+from ete_dev.treeview.main import TreeImage
+
+def ly(node):
+    node.img_style["size"] = 4
+    node.img_style["shape"] = "square"
+
+I = TreeImage()
+I.mode = "circular"
+I.scale = 50
+
+n = int(sys.argv[1])
+
+t = Tree()
+t.populate(n)
+t.show(ly, img_properties=I)
