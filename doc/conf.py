c54b88aae24da56b9d343ef01452c56bf3070b55
Jaime Huerta-Cepas <jhcepas@gmail.com>
2011-05-05 15:07:42 +0200

    exclude source code from doc pages

--- doc/conf.py
+++ doc/conf.py
@@ -23,7 +23,9 @@ sys.path.insert(0, os.path.abspath('../'))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', \
-                  'sphinx.ext.autosummary', 'sphinx.ext.viewcode']
+                  'sphinx.ext.autosummary']
+
+#, 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['templates']
