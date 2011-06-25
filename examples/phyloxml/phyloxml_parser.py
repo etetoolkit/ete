d5e7526147274fbea3c7a858447327d524d886d3
Jaime Huerta-Cepas <jhcepas@gmail.com>
2011-06-25 10:57:03 +0200

    another fix in phyloxml tuto

--- examples/phyloxml/phyloxml_parser.py
+++ examples/phyloxml/phyloxml_parser.py
@@ -9,9 +9,8 @@ for tree in project.get_phylogeny():
     tree.show()
     # PhyloXML features are stored in the phyloxml_clade attribute
     for node in tree: 
-        if node.phyloxml_clade.get_sequence(): 
-            print "Node name:", node.name
-            for seq in node.phyloxml_clade.sequence: 
-                for domain in seq.domain_architecture.get_domain():
-                    domain_data = [domain.valueOf_, domain.get_from(), domain.get_to()]
-                    print "   ", '\t'.join(map(str, domain_data))
+        print "Node name:", node.name
+        for seq in node.phyloxml_clade.get_sequence(): 
+            for domain in seq.domain_architecture.get_domain():
+                domain_data = [domain.valueOf_, domain.get_from(), domain.get_to()]
+                print "  Domain:", '\t'.join(map(str, domain_data))
