93496e189ec06153c5472747498c6a1650ef06a2
Jaime Huerta-Cepas <jhcepas@gmail.com>
2013-06-29 09:32:40 +0200

    fix problems reported in the mailing list about face rotation

--- ete_dev/treeview/faces.py
+++ ete_dev/treeview/faces.py
@@ -150,7 +150,8 @@ class Face(object):
         self.border = _Border()
         self.inner_border = _Border()
         self.inner_background = _Background()
-
+        self.rotation = 0
+        
     def _size(self):
         if self.pixmap:
             return self._width(),self._height()
@@ -218,7 +219,7 @@ class TextFace(Face):
         return self._text
         
     def _set_text(self, txt):
-        self._text = txt
+        self._text = str(txt)
         
     def get_bounding_rect(self):
         if not self._bounding_rect:
@@ -247,8 +248,7 @@ class TextFace(Face):
         self.fstyle = fstyle
         self.penwidth = penwidth
         self.tight_text = tight_text
-        if text:
-            self.text = text
+        self.text = text
 
     def _get_font(self):
         font = QFont(self.ftype, self.fsize)
