3496132d859f1468b4504cd1fcd7c20844727ef1
Jaime Huerta-Cepas <jhcepas@gmail.com>
2013-11-15 10:09:38 +0100

    parallel version of maptrees

--- ete_tools/common.py
+++ ete_tools/common.py
@@ -1,10 +1,7 @@
 import sys
 
-sys.path.insert(0, "/home/huerta/_Devel/ete_master")
 from ete_dev import Tree, PhyloTree, faces, TreeStyle, add_face_to_node, orthoxml, SVG_COLORS, faces, treeview, NodeStyle
-sys.path.insert(0, "/home/huerta/_Devel/jhc-utils")
-from jhc_utils import print_table, color
-
+from jhc_utils import print_table, color, timeit
 from ncbi_taxonomy import ncbi_query
 
 try:
