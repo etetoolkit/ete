d2bd9e6265c10c97b23dfdebacec1b9a14221a37
Jaime Huerta-Cepas <jhcepas@gmail.com>
2014-02-21 15:29:17 +0100

    clean up before merge

--- ete_tools/test/maptrees/run_tests.sh
+++
@@ -1,5 +0,0 @@
-for x in test*
-do
- (cd $x && rm *.png; ete maptrees -r reftree --source_trees genetrees -o annotated_reftree --debug)
-done
-
--- ete_tools/test/maptrees/test_constrains/run.sh
+++
@@ -1,2 +0,0 @@
-rm *.png; ete maptrees -r reftree --source_trees genetrees --output annotated_reftree.pkl --reftree_constraint constraint.py --debug
-
