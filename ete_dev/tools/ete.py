9379fac3fef9c8fed7e201097832893fed8914f1
Jaime Huerta-Cepas <jhcepas@gmail.com>
2015-02-23 11:49:53 +0100

    about to change

--- ete_dev/tools/ete.py
+++ ete_dev/tools/ete.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import sys
-import argparse
+TOOLSPATH = os.path.split(os.path.realpath(__file__))[0]
+sys.path.insert(0, TOOLSPATH)
 
+import argparse
 import ete_split, ete_expand, ete_annotate, ete_ncbiquery, ete_view, ete_generate, ete_mod, ete_extract, ete_compare
 import common
 from common import log
