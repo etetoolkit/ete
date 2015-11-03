c05c60d3dd18af9be6dffd4601be7b47093c667b
Jaime Huerta-Cepas <jhcepas@gmail.com>
2015-11-03 16:42:02 +0100

    better external tool installer

--- ete3/tools/ete.py
+++ ete3/tools/ete.py
@@ -44,11 +44,6 @@ from __future__ import absolute_import
 from __future__ import print_function
 import sys
 import os
-from os.path import join as pjoin
-from os.path import split as psplit
-from os.path import exists as pexist
-
-from six.moves import input
 
 TOOLSPATH = os.path.realpath(os.path.split(os.path.realpath(__file__))[0])
 #sys.path.insert(0, TOOLSPATH)
@@ -101,54 +96,10 @@ def main():
             print(__version__)
             return
         elif subcommand == "upgrade-external-tools":
-            import tarfile
-            try:
-                from urllib import urlretrieve
-            except ImportError:
-                # python 3
-                from urllib.request import urlretrieve
-
-            
-            APPSPATH = os.path.expanduser("~/.etetoolkit/ext_apps-latest/")
-            ETEHOMEDIR = os.path.expanduser("~/.etetoolkit/")
-            
-            if os.path.exists(pjoin('/etc/etetoolkit/', 'ext_apps-latest')):
-                # if a copy of apps is part of the ete distro, use if by default
-                APPSPATH = pjoin('/etc/etetoolkit/', 'ext_apps-latest')
-                ETEHOMEDIR = '/etc/etetoolkit/'
-            else:
-                # if not, try a user local copy
-                APPSPATH = pjoin(ETEHOMEDIR, 'ext_apps-latest')
-
-            print (colorify('Downloading latest version of tools...', "green"), file=sys.stderr)
-
-            if len(sys.argv) > 2:
-                TARGET_DIR = sys.argv[2]
-            else:
-                TARGET_DIR = ''
-                
-            while not pexist(TARGET_DIR):
-                TARGET_DIR = input('target directory? [%s]:' %ETEHOMEDIR).strip()
-                if TARGET_DIR == '':
-                    TARGET_DIR = ETEHOMEDIR
-                    break
-                
-            if TARGET_DIR == ETEHOMEDIR:
-                try:
-                    os.mkdir(ETEHOMEDIR)
-                except OSError:
-                    pass
-
-            version_file = "latest.tar.gz"
-            urlretrieve("https://github.com/jhcepas/ext_apps/archive/%s" %version_file, pjoin(TARGET_DIR, version_file))
-            print(colorify('Decompressing...', "green"), file=sys.stderr)
-            tfile = tarfile.open(pjoin(TARGET_DIR, version_file), 'r:gz')
-            tfile.extractall(TARGET_DIR)
-            print(colorify('Compiling tools...', "green"), file=sys.stderr)
-            sys.path.insert(0, pjoin(TARGET_DIR, 'ext_apps-latest'))
-            import compile_all
-            s = compile_all.compile_all()
-            sys.exit(s)
+            from . import ete_upgrade_tools
+            del sys.argv[1]
+            status = ete_upgrade_tools._main()
+            sys.exit(status)
             
         elif subcommand == "build":
             from . import phylobuild
