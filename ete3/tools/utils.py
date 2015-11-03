6f8f85d210e34dc5dccfce88a30bd097007a54fc
Jaime Huerta-Cepas <jhcepas@gmail.com>
2015-11-03 13:21:43 +0100

    added utils module to ete-tools

---
+++ ete3/tools/utils.py
@@ -0,0 +1,27 @@
+# CONVERT shell colors to the same curses palette
+COLORS = {
+    "wr": '\033[1;37;41m', # white on red
+    "wo": '\033[1;37;43m', # white on orange
+    "wm": '\033[1;37;45m', # white on magenta
+    "wb": '\033[1;37;46m', # white on blue
+    "bw": '\033[1;37;40m', # black on white
+    "lblue": '\033[1;34m', # light blue
+    "lred": '\033[1;31m', # light red
+    "lgreen": '\033[1;32m', # light green
+    "yellow": '\033[1;33m', # yellow
+    "cyan": '\033[36m', # cyan
+    "blue": '\033[34m', # blue
+    "green": '\033[32m', # green
+    "orange": '\033[33m', # orange
+    "red": '\033[31m', # red
+    "magenta": "\033[35m", # magenta
+    "white": "\033[0m", # white
+    None: "\033[0m", # end
+}
+
+def colorify(string, color):
+    return "%s%s%s" %(COLORS[color], string, COLORS[None])
+
+def clear_color(string):
+    return re.sub("\\033\[[^m]+m", "", string)
+
