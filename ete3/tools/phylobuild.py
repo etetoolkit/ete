d2fa9f2bac0e97f95e3a289610f28e8aca21dd91
Jaime Huerta-Cepas <jhcepas@gmail.com>
2016-01-15 14:31:18 +0100

    ete build: preparing to read binaries from bundle

--- ete3/tools/phylobuild.py
+++ ete3/tools/phylobuild.py
@@ -644,7 +644,7 @@ def _main(arguments, builtin_apps_path=None):
     global BASEPATH, APPSPATH, args
     
     if builtin_apps_path:
-        APPSPATH = os.abspath(builtin_apps_path)
+        APPSPATH = builtin_apps_path
 
     if not pexist(APPSPATH, "bin"):
         APPSPATH = os.path.expanduser("~/.etetoolkit/ext_apps-latest/")
