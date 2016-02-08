9c4e5743f65bf740212ff391ebf36a93923cca30
Jaime Huerta-Cepas <jhcepas@gmail.com>
2016-02-08 11:11:15 +0100

    corrects error in ete_evol_test caused by stop codon in alg

--- ete3/test/test_ete_evol.py
+++ ete3/test/test_ete_evol.py
@@ -9,18 +9,19 @@ import unittest
 from ..tools import ete
 from ..evol.control import AVAIL
 
-DATAPATH = os.path.abspath(os.path.split(os.path.realpath(__file__))[0])+"/ete_evol_data/S_example/"
-SDATAPATH = os.path.abspath(os.path.split(os.path.realpath(__file__))[0])+"/ete_evol_data/XS_example/"
-OUTPATH = '/tmp/ete3_evol-test/'
+DATAPATH = os.path.abspath(os.path.split(os.path.realpath(__file__))[0])+"/ete_evol_data/S_example"
+SDATAPATH = os.path.abspath(os.path.split(os.path.realpath(__file__))[0])+"/ete_evol_data/XS_example"
+OUTPATH = 'tmp/ete3_evol-test/'
 
 class Test_ete_evol(unittest.TestCase):
     def test_01_all_models(self):
         models = [k for k in AVAIL.keys() if not k.startswith('XX.') and not k == 'SLR']
-
+        
         cmd = 'ete3 evol --mark Pan_troglodytes --noimg --alg %s/ali.fasta -t %s/tree.nw --clear_all -o %s --cpu %d --model ' %(
             SDATAPATH, SDATAPATH, OUTPATH, CPUS)
         args = cmd.split()
         args.extend([model for model in models if not 'XX' in model])
+        print(' '.join(args))
         ete._main(args)
 
     def test_02_next_test_to_be_defined(self):
