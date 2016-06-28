15122a0a84b1fcc2dd8cbfacd862384bf541d931
Alisha Mechtley <acr11n@my.fsu.edu>
2016-06-28 01:44:19 -0400

    Store more than one attribute in the dictionary when provided. Also added parameter: descendant_leaves.

--- ete3/coretype/tree.py
+++ ete3/coretype/tree.py
@@ -1552,7 +1552,7 @@ class TreeNode(object):
             if not n.is_leaf():
                 n.children.sort(key=lambda x: str(sorted(node2content[x])))
 
-    def get_cached_content(self, store_attr=None, container_type=set, _store=None):
+    def get_cached_content(self, store_attr=None, container_type=set, _store=None, descendant_leaves=True):
         """
         .. versionadded: 2.2
 
@@ -1568,30 +1568,42 @@ class TreeNode(object):
         :param _store: (internal use)
 
         """
+
         if _store is None:
             _store = {}
 
         for ch in self.children:
             ch.get_cached_content(store_attr=store_attr,
                                   container_type=container_type,
-                                  _store=_store)
-        if self.children:
-            val = container_type()
-            for ch in self.children:
-                if type(val) == list:
-                    val.extend(_store[ch])
-                if type(val) == set:
-                    val.update(_store[ch])
-            _store[self] = val
+                                  _store=_store, leaves_only=leaves_only)
+        if descendant_leaves:
+            if self.children:
+                val = container_type()
+                for ch in self.chil
+                    if type(val) == list:
+                        val.extend(_store[ch])
+                    if type(val) == set:
+                        val.update(_store[ch])
+                _store[self] = val
+            else:
+                if store_attr is None:
+                    val = [self]
+                else:
+                    if not isinstance(store_attr, six.string_types):
+                        val = [tuple(getattr(self, attr, None) for attr in store_attr)]
+                    else:
+                        val = [getattr(self, store_attr, None)]
+                _store[self] = container_type(val)
         else:
             if store_attr is None:
-                val = [self]
+                val = self
             else:
-                try:
-                    val = [getattr(self, store_attr)]
-                except AttributeError:
-                    val = []
-            _store[self] = container_type(val)
+                if not isinstance(store_attr, six.string_types):
+                    val = [tuple(getattr(self, attr, None) for attr in store_attr)]
+                else:
+                    val = [getattr(self, store_attr, None)]
+            _store[self] = val
+
         return _store
 
     def robinson_foulds(self, t2, attr_t1="name", attr_t2="name",
--- ete3/tools/ete_treematcher.py
+++ ete3/tools/ete_treematcher.py
@@ -1,4 +1,8 @@
+from .common import log
+
 DESC=''
+
+#ete3 treematcher --pattern "(hello, kk);" --pattern-format 8 --tree-format 8 --trees "(hello,(1,2,3)kk);" --quoted-node-names
 
 
 def populate_args(extract_args_p):
@@ -7,7 +11,7 @@ def populate_args(extract_args_p):
     extract_args.add_argument("--pattern", dest="pattern",
                               nargs=1,
                               help="")
-    extract_args.add_argument("--format", dest="format",
+    extract_args.add_argument("--pattern-format", dest="pattern_format",
                               nargs=1,
                               help="")
     extract_args.add_argument("--quoted-node-names", dest="quoted_node_names",
@@ -17,22 +21,26 @@ def populate_args(extract_args_p):
     extract_args.add_argument("--trees", dest="trees",
                               nargs="*",
                               help="")
+    extract_args.add_argument("--tree-format", dest="tree_format",
+                              nargs=1,
+                              help="")
 
 
 def run(args):
-    from .. import Tree, PhyloTree, treematcher
-    #from ete3 import Tree, PhyloTree
+    from .. import Tree, PhyloTree
     import treematcher
 
     if args.pattern is not None:
         p = args.pattern[0]
-        pattern = treematcher.TreePattern(p, format=int(args.format[0]), quoted_node_names=args.quoted_node_names)
+        pattern = treematcher.TreePattern(p, format=int(args.pattern_format[0]), quoted_node_names=args.quoted_node_names)
         if args.trees is not None:
             for t in args.trees:
-                t = Tree(t, format=int(args.format[0]), quoted_node_names=args.quoted_node_names)
+                t = Tree(t, format=int(args.tree_format[0]), quoted_node_names=args.quoted_node_names)
                 print pattern.find_match(t, None)
 
         else:
-            print "please specify a tree"
+            log.error('Please specify a tree to search.')
+            sys.exit(-1)
     else:
-        print "please specify a tree pattern"
+        log.error('Please specify a pattern to search for.')
+        sys.exit(-1)
