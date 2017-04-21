55b6e60dd9c33041304e288a1ecb92eaee818149
Jaime Huerta-Cepas <jhcepas@gmail.com>
2017-04-21 15:26:38 +0200

    really fixes problem exporting quoted names in newick format

--- ete3/parser/newick.py
+++ ete3/parser/newick.py
@@ -169,7 +169,7 @@ def format_node(node, node_type, format, dist_formatter=None,
         FIRST_PART = name_formatter %FIRST_PART
         if quoted_names:
             #FIRST_PART = '"%s"' %FIRST_PART.decode('string_escape').replace('"', '\\"')
-            FIRST_PART = '"%s"' %FIRST_PART.replace('"', '\\"')
+            FIRST_PART = '"%s"' %FIRST_PART
 
     elif converterFn1 is None:
         FIRST_PART = ""
