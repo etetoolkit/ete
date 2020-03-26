53b6498f5dae74be142c55d9a5c46b5430d9d69b
dengzq1234 <dengziqi1234@gmail.com>
2020-03-26 18:50:57 +0100

    undo fix

--- ete3/tools/ete_build_lib/task/iqtree.py
+++ ete3/tools/ete_build_lib/task/iqtree.py
@@ -29,7 +29,7 @@ class IQTree(TreeTask):
             base_args["-st"] = "AA" if seqtype == "aa" else "DNA"            
         else:
             if conf[confname]["-st"].startswith("CODON"):
-                if seqtype == "aa" or "aa" in GLOBALS["seqtypes"]: 
+                if seqtype == "aa" or "aa" not in GLOBALS["seqtypes"]: 
                     raise ConfigError("IQTREE CODON models require a codon alignment.\nProvide nucleotide sequences with '-n' and set '--nt-switch-thr 0.0' to ensure codon alignments.")
             
         if model:
