30136f8888cbd65d60a18a1bd7e74c91137bdec0
jorgebotas <jorgebotasmiret@protonmail.com>
2021-04-29 16:38:35 +0200

    Updated NewickError

--- ete4/parser/newick.pyx
+++ ete4/parser/newick.pyx
@@ -1,9 +1,52 @@
+# #START_LICENSE###########################################################
+#
+#
+# This file is part of the Environment for Tree Exploration program
+# (ETE).  http://etetoolkit.org
+#
+# ETE is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ETE is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+# License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ETE.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#                     ABOUT THE ETE PACKAGE
+#                     =====================
+#
+# ETE is distributed under the GPL copyleft license (2008-2015).
+#
+# If you make use of ETE in published work, please cite:
+#
+# Jaime Huerta-Cepas, Joaquin Dopazo and Toni Gabaldon.
+# ETE: a python Environment for Tree Exploration. Jaime BMC
+# Bioinformatics 2010,:24doi:10.1186/1471-2105-11-24
+#
+# Note that extra references to the specific methods implemented in
+# the toolkit may be available in the documentation.
+#
+# More info at http://etetoolkit.org. Contact: huerta@embl.de
+#
+#
+# #END_LICENSE#############################################################
 """ Jordi's newick parser """
 from ete4 import Tree
 
 
 class NewickError(Exception):
-    pass
+    """Exception class designed for NewickIO errors."""
+    def __init__(self, value):
+        if value is None:
+            value = ''
+        value += "\nYou may want to check the input Newick format."
+        Exception.__init__(self, value)
 
 
 DIST_FORMAT = '%g'  # format used to represent the dist as a string
