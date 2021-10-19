5b4db1c0805147639502612dcc9df917529264ef
jorgebotas <jorgebotasmiret@protonmail.com>
2021-10-19 13:22:26 +0200

    parent event

--- ete4/smartview/gui/static/js/select.js
+++ ete4/smartview/gui/static/js/select.js
@@ -10,36 +10,36 @@ function select_node(node_id, name) {
 
     parent.postMessage({ selected: [node_id] }, "*");
 
-    if (name in view.selection) {
-        view.selection[name].nodes.push(node_id);
+    if (name in view.selected) {
+        view.selected[name].nodes.push(node_id);
         colorize_selection(name);
         return true;
     }
 
     const folder = menus.selection.addFolder({ title: name, expanded: false });
     const colors = ["#FF0", "#F0F", "#0FF", "#F00", "#0F0", "#00F"].reverse();
-    const nselected = Object.keys(view.selection).length;
-    view.selection[name] = {
+    const nselected = Object.keys(view.selected).length;
+    view.selected[name] = {
         nodes: [node_id],
         opacity: 0.4,
         color: colors[nselected % colors.length],
     };
 
-    view.selection[name].remove = function() {
-        view.selection[name].opacity = view.node.box.opacity;
-        view.selection[name].color = view.node.box.color;
+    view.selected[name].remove = function() {
+        view.selected[name].opacity = view.node.box.opacity;
+        view.selected[name].color = view.node.box.color;
         colorize_selection(name);
-        parent.postMessage({ unselected: view.selection[name].nodes }, "*");
-        delete view.selection[name];
+        parent.postMessage({ unselected: view.selected[name].nodes }, "*");
+        delete view.selected[name];
         folder.dispose();
     }
 
-    folder.addInput(view.selection[name], "opacity", { min: 0, max: 1, step: 0.1 })
+    folder.addInput(view.selected[name], "opacity", { min: 0, max: 1, step: 0.1 })
         .on("change", () => colorize_selection(name));
-    folder.addInput(view.selection[name], "color", { view: "color" })
+    folder.addInput(view.selected[name], "color", { view: "color" })
         .on("change", () => colorize_selection(name));
 
-    folder.addButton({ title: "remove" }).on("click", view.selection[name].remove);
+    folder.addButton({ title: "remove" }).on("click", view.selected[name].remove);
 
     colorize_selection(name);
 
@@ -48,7 +48,7 @@ function select_node(node_id, name) {
 }
 
 function colorize_selection(name) {
-    const selection = view.selection[name];
+    const selection = view.selected[name];
     selection.nodes.forEach(node_id => {
         const node = document.getElementById("node-" + node_id.join("_"));
         if (node) {
