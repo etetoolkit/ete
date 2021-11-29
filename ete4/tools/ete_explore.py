2f7f5e0a79bd6099a09b3ccf452f22a376c62e61
Jaime Huerta Cepas <jhcepas@gmail.com>
2021-11-29 11:05:19 +0100

    "ete4 explore" opens a local server to upload trees

--- ete4/tools/ete_explore.py
+++ ete4/tools/ete_explore.py
@@ -47,33 +47,31 @@ from collections import defaultdict
 from .common import log, POSNAMES, node_matcher, src_tree_iterator
 # from .. import (Tree, PhyloTree, TextFace, RectFace, faces, TreeStyle, CircleFace, AttrFace,
 #                 add_face_to_node, random_color)
-from ete4 import Tree, NodeStyle, PhyloTree
-from ete4.smartview import TreeStyle
-from six.moves import map
+from .. import PhyloTree
+from ..smartview import TreeStyle
+from ..smartview.gui.server import run_smartview
 
-DESC = ""
-FACES = []
+DESC = "Launches an instance of the ETE smartview tree explorer server."
 
 def populate_args(explore_args_p):
     explore_args_p.add_argument("--face", action="append",
                              help="adds a face to the selected nodes. In example --face 'value:@dist, pos:b-top, color:red, size:10, if:@dist>0.9' ")
-
     return
 
+
+
 def run(args):
-
-    global FACES
-
-    if args.face:
-        FACES = parse_faces(args.face)
-    else:
-        FACES = []
     
     # VISUALIZATION
     # Basic tree style
     ts = TreeStyle()
     ts.show_leaf_name = True
-
-    for tindex, tfile in enumerate(src_tree_iterator(args)):
+    try:
+        tfile = next(src_tree_iterator(args))
+    except StopIteration:
+        run_smartview()
+    else:
         t = PhyloTree(tfile, format=args.src_newick_format)
-        t.explore(tree_name="example", tree_style=ts)
+        t.explore(tree_name=tfile, tree_style=ts)
+ 
+        
