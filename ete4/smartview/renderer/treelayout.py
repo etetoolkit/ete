2c838b356157f8f0560d4b1234760bc488e55b57
jorgebotas <jorgebotasmiret@protonmail.com>
2021-11-29 12:56:21 +0100

    self.ns

---
+++ ete4/smartview/renderer/treelayout.py
@@ -0,0 +1,25 @@
+class TreeLayout():
+    def __init__(self, name, ns=None, ts=None):
+        self.name = name
+        self.active = True
+        self.aligned_faces = False
+        self.description = ""
+
+        self.always_render = False
+
+        self.ts = ts
+        self.ns = ns
+
+    def set_tree_style(self, style):
+        if self.aligned_faces:
+            style.aligned_panel = True
+
+        if self.ts:
+            self.ts(style)
+    
+    def set_node_style(self, node):
+        if self.ns:
+            self.ns(node)
+        
+    def post_render(self):
+        pass
