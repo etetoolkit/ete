bf86e5afa6df470a55993c190428895aa853ba86
jorgebotas <jorgebotasmiret@protonmail.com>
2021-11-29 12:27:34 +0100

    NodeStyle import in smartview

--- ete4/smartview/__init__.py
+++ ete4/smartview/__init__.py
@@ -1,5 +1,6 @@
 from .renderer.faces import *
 from .renderer.face_positions import *
 from .renderer.treestyle import TreeStyle
+from .renderer.nodestyle import NodeStyle
 from .renderer.layouts.default_layouts import *
 from .renderer import layouts as layout_modules
