d5396c555bb987c98d023152f600fb5c30b3e791
jorgebotas <jorgebotasmiret@protonmail.com>
2021-12-02 10:20:29 +0100

    conservation

--- ete4/smartview/renderer/layouts/context_layouts.py
+++ ete4/smartview/renderer/layouts/context_layouts.py
@@ -9,6 +9,7 @@ class LayoutGenomicContext(TreeLayout):
 
     def __init__(self, name="Genomic context", nside=2,
             conservation_threshold=0, width=70, height=15, collapse_size=1,
+            gene_name="name",
             stroke_color="gray", stroke_width="1.5px",
             anchor_stroke_color="black", anchor_stroke_width="3px",
             non_conserved_color="#d0d0d0"):
@@ -17,6 +18,7 @@ class LayoutGenomicContext(TreeLayout):
 
         self.nside = nside
         self.conservation_threshold = conservation_threshold
+        self.gene_name
 
         self.width = width
         self.height = height
@@ -43,7 +45,7 @@ class LayoutGenomicContext(TreeLayout):
         if context:
             for idx, gene in enumerate(context):
                 name = gene.get("name")
-                color = gene.get("color", "gray")
+                color = gene.get("color", self.non_conserved_color)
                 conservation = gene.get("conservation_score")
                 if conservation is not None\
                     and float(conservation) >= self.conservation_threshold:
@@ -51,14 +53,14 @@ class LayoutGenomicContext(TreeLayout):
                 strand = gene.get("strand", "+")
                 cluster = gene.get("cluster")
                 orientation = "left" if strand == "-" else "right"
-                text = name + " #" + cluster
+                text = gene.get(self.gene_name, "")
                 if idx == self.nside:
                     stroke_color = self.anchor_stroke_color
                     stroke_width = self.anchor_stroke_width
                 else:
                     stroke_color = self.stroke_color
                     stroke_width = self.stroke_width
-                props = {"name": name, "cluster": cluster}
+                props = { k:v for k,v in gene.items() if k not in ("strand", "color") }
                 tooltip = "\n".join(f'{k}: {v}' for k,v in props.items())
                 arrow = ArrowFace(self.width, self.height,
                         orientation=orientation, color=color,
