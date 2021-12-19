467bfd21b3b2fc3828937b5024e717a2d4086dab
jorgebotas <jorgebotasmiret@protonmail.com>
2021-12-19 16:49:56 +0100

    newick endpoint

--- ete4/smartview/renderer/layouts/__init__.py
+++ ete4/smartview/renderer/layouts/__init__.py
@@ -1,4 +1,5 @@
 from . import context_layouts
 from . import evol_events_layouts
+from . import etecompare_layouts
 from . import ncbi_taxonomy_layouts
 from . import seq_layouts
---
+++ ete4/smartview/renderer/layouts/etecompare_layouts.py
@@ -0,0 +1,18 @@
+from ..treelayout import TreeLayout
+
+
+__all__ = [ "LayoutEteDiffDistance" ]
+
+
+class LayoutEteDiffDistance(TreeLayout):
+    def __init__(self, name="ETE diff distance", diff_node_color="#a50000"):
+        super().__init__(name)
+
+        self.diff_node_color = diff_node_color
+
+    def set_node_style(self, node):
+        difference = node.props.get("distance")
+        if difference:
+            node.sm_style["fgcolor"] = self.diff_node_color
+            node.sm_style["size"] = 2
+            node.sm_style["opacity"] = float(difference)
