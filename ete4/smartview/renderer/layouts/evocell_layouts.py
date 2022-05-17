2d30e7d1b4c96f870118ff60b318d709035d3b36
jorgebotas <jorgebotasmiret@protonmail.com>
2022-05-17 12:15:03 +0200

    evocell_layouts reimplementation to new TreeLayout system

--- ete4/smartview/renderer/layouts/evocell_layouts.py
+++ ete4/smartview/renderer/layouts/evocell_layouts.py
@@ -1,35 +1,42 @@
 from ..faces import RectFace, TextFace
+from ..treelayout import TreeLayout
 
-__all__ = [ "get_layout_human_og", "get_layout_UCSC"]
+__all__ = [ "LayoutHumanOGs", "LayoutUCSC"]
 
-def get_layout_human_og():
-    column = 1
-    rect_width = 15
-    def layout_fn(node):
-        
+
+class LayoutHumanOGs(TreeLayout):
+    def __init__(self, name="Human OGs", human_orth_prop="human_orth",
+                 column=4, color="#6b92d6"):
+        super().__init__(name)
+        self.aligned_faces = True
+        self.human_orth_prop = human_orth_prop
+        self.column = column
+        self.color = color
+
+    def set_node_style(self, node):
         if node.is_leaf():
-            if node.props.get('human_orth'):
-                human_orth = " ".join(node.props.get('human_orth').split('|'))
-                human_orth_face = TextFace(human_orth, color="blue")
-                node.add_face(human_orth_face, column = 4, position = "aligned")
+            human_orth = node.props.get(self.human_orth_prop)
+            if human_orth:
+                human_orth = " ".join(human_orth.split('|'))
+                human_orth_face = TextFace(human_orth, color=self.color)
+                node.add_face(human_orth_face, column=self.column, position="aligned")
 
-    layout_fn.__name__ = 'Human OGs'
-    layout_fn.contains_aligned_face = True
-    return layout_fn
+class LayoutUCSC(TreeLayout):
+    def __init__(self, name="UCSC", column=5, 
+                 nodecolor="#3f8c3f", nodesize=5,
+                 textcolor="#c43b5d"):
+        super().__init__(name)
+        self.aligned_faces = True
+        self.column = column
+        self.nodecolor = nodecolor
+        self.nodesize = nodesize
+        self.textcolor = textcolor
 
-def get_layout_UCSC():
-    column = 1
-    rect_width = 15
-    def layout_fn(node):
-        
+    def set_node_style(self, node):
         if node.is_leaf():
             if node.props.get('UCSC'):
                 ucsc = node.props.get('UCSC')
-                ucsc_face = TextFace(ucsc, color="red")
-                node.add_face(ucsc_face, column = 5, position = "aligned")
-                node.sm_style["fgcolor"] = "green"
-                node.sm_style["size"] = 10
-
-    layout_fn.__name__ = 'UCSC'
-    layout_fn.contains_aligned_face = True
-    return layout_fn
+                ucsc_face = TextFace(ucsc, color=self.textcolor)
+                node.add_face(ucsc_face, column=self.column, position="aligned")
+                node.sm_style["fgcolor"] = self.nodecolor
+                node.sm_style["size"] = self.nodesize
