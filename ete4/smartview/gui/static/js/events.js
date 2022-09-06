552b27b67391f8e6131c8e36fe695c609fceee26
jorgebotas <jorgebotasmiret@protonmail.com>
2022-09-06 13:17:35 +0200

    Pickle POST

--- ete4/smartview/gui/static/js/events.js
+++ ete4/smartview/gui/static/js/events.js
@@ -143,7 +143,7 @@ function get_event_zoom(event) {
 function on_wheel(event) {
     const g_panel0 = div_tree.children[0].children[0];
 
-    if (div_tree === event.target)
+    if (div_tree.contains(event.target))
         event.preventDefault();
 
     if (!is_svg(event.target) || g_panel0.contains(event.target))
