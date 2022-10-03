be018bb7250cb63c074663b2e1aa67c4168313b5
jorgebotas <jorgebotasmiret@protonmail.com>
2022-10-03 08:26:06 -0500

    Evolutionary events legend

--- ete4/smartview/renderer/layouts/evol_events_layouts.py
+++ ete4/smartview/renderer/layouts/evol_events_layouts.py
@@ -7,22 +7,26 @@ __all__ = [ "LayoutEvolEvents" ]
 class LayoutEvolEvents(TreeLayout):
     def __init__(self, name="Evolutionary events", 
             prop="evol_event",
-            speciation_color="blue", duplication_color="red"):
+            speciation_color="blue", 
+            duplication_color="red"
+            legend=True):
         super().__init__(name)
         
         self.prop = prop
         self.speciation_color = speciation_color
         self.duplication_color = duplication_color
+        self.legend = legend
 
         self.active = True
 
     def set_tree_style(self, tree, tree_style):
         super().set_tree_style(tree, tree_style)
-        colormap = { "Speciation event": self.speciation_color,
-                     "Duplication event": self.duplication_color }
-        tree_style.add_legend(title=self.name, 
-                variable="discrete",
-                colormap=colormap)
+        if self.legend:
+            colormap = { "Speciation event": self.speciation_color,
+                         "Duplication event": self.duplication_color }
+            tree_style.add_legend(title=self.name, 
+                    variable="discrete",
+                    colormap=colormap)
 
     def set_node_style(self, node):
         if not node.is_leaf():
