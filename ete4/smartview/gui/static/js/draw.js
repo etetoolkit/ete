8f79791c243c3821741a961474a15d641918c3fc
Jordi <jordi.burguet.castell@gmail.com>
2023-02-21 22:12:11 +0100

    cosmetics
    Many are just whitespace cleanup (which happened atuomatically
    after opening and saving the file).

--- ete4/smartview/gui/static/js/draw.js
+++ ete4/smartview/gui/static/js/draw.js
@@ -26,7 +26,7 @@ function get_tree_params() {
     const [zx, zy, za] = [view.zoom.x, view.zoom.y, view.zoom.a];
     const [x, y] = [view.tl.x, view.tl.y];
     const [w, h] = [div_tree.offsetWidth / zx, div_tree.offsetHeight / zy];
-    
+
     const layouts = JSON.stringify(get_active_layouts());
 
     const params_rect = {  // parameters we have to pass to the drawer
@@ -76,7 +76,7 @@ async function draw_tree() {
         await draw_legend(params)
 
         if (view.drawer.npanels > 1) {
-            align_timeout = setTimeout(async () => { 
+            align_timeout = setTimeout(async () => {
                 if (!align_drawing)
                     await draw_aligned(params);
                 clearTimeout(align_timeout);
@@ -127,9 +127,9 @@ function draw_tree_scale() {
             "stroke-width": view.tree_scale.width,
         });
     }
-    
+
     function create_text(text) {
-        const t = create_svg_element("text", { 
+        const t = create_svg_element("text", {
             "font-size": view.tree_scale.fsize,
             "fill": view.tree_scale.color,
             x: x0 + length + 10,
@@ -189,16 +189,17 @@ function draw(element, items, tl, zoom, replace=true) {
 
     const svg_items = items.filter(is_svg);
     svg_items.forEach(item => g.appendChild(create_item(g, item, tl, zoom)));
-    
+
     const pixi_container = view.drawer.type === "circ" ? div_tree : element;
     const pixi_items = items.filter(i => i[0].includes("pixi-"));
-    if (pixi_items.length) {
+    if (pixi_items.length > 0) {
         const pixi = draw_pixi(pixi_container, pixi_items, tl, zoom)
         pixi.style.transform = "translate(0, 0)";
         replace_child(pixi_container.querySelector(".div_pixi"), pixi);
 
-    } else if (replace)
+    } else if (replace) {
         clear_pixi(pixi_container);
+    }
 
     const html_container = element.querySelector(".div_html")
     if (html_container) {
@@ -309,14 +310,14 @@ async function draw_aligned(params, npanels) {
 
     const zx = view.zoom.a;
     const zy = view.zoom.y;
-    const panels = { 
+    const panels = {
         1: { div: div_aligned, show: true,
-             params: { x: view.aligned.x, y: view.tl.y, w: div_aligned.offsetWidth / zx } }, 
+             params: { x: view.aligned.x, y: view.tl.y, w: div_aligned.offsetWidth / zx } },
 
         2: { div: div_aligned_header, show: view.aligned.header.show,
              params: { x: view.aligned.x, y: 0,
                  w: div_aligned.offsetWidth / zx,
-                 h: Math.max(-view.tl.y, view.aligned.header.height / zy) } }, 
+                 h: Math.max(-view.tl.y, view.aligned.header.height / zy) } },
 
         3: { div: div_aligned_footer, show: view.aligned.footer.show,
              params: { x: view.aligned.x, y: 0,
@@ -354,7 +355,7 @@ async function draw_aligned(params, npanels) {
 
             // NOTE: Only implemented for panel=1 for the moment. We just need to
             //   decide where the graphics would go for panel > 1 (another div? ...)
-            
+
         }
     }
     else {
@@ -386,7 +387,7 @@ function get_node_class(t) {
 function create_item(g, item, tl, zoom) {
     // item looks like ["line", ...] for a line, etc.
 
-    const [zx, zy] = [zoom.x, zoom.y];  // shortcut 
+    const [zx, zy] = [zoom.x, zoom.y];  // shortcut
     if (item[0] === "nodebox") {
         const [ , box, name, properties, node_id, result_of, style] = item;
 
@@ -484,7 +485,7 @@ function create_item(g, item, tl, zoom) {
             const angle = Math.atan2(zy * tl.y + cy, zx * tl.x + cx) * 180 / Math.PI;
             addRotation(ellipse, angle, cx, cy);
         }
-        
+
         style_polygon(ellipse, style);
 
         ellipse.setAttribute("data-tooltip", tooltip);
@@ -497,11 +498,11 @@ function create_item(g, item, tl, zoom) {
         const triangle = create_triangle(box, tip, tl, zx, zy, type);
 
         const [r, a, dr, da] = box;
-        if (view.drawer.type === "circ" 
+        if (view.drawer.type === "circ"
             && ["top", "bottom"].includes(tip)
             && (Math.abs(a + da) > Math.PI / 2)) {
             const {x: cx, y: cy} = cartesian_shifted(r + dr / 2,
-                                                     a + da / 2, 
+                                                     a + da / 2,
                                                      tl, zx);
             addRotation(triangle, 180, cx, cy);
         }
@@ -596,12 +597,12 @@ function create_html(container, item, tl, zoom) {
 
     const [x, y, dx, dy] = item[1];
     const [zx, zy] = [zoom.x, zoom.y];
-    const style = { 
+    const style = {
         position: "absolute",
         overflow: "hidden",
         top: zy * (y - tl.y) + "px",
         left: zx * (x - tl.x) + "px",
-        width: dx * zx + "px", 
+        width: dx * zx + "px",
         height: dy * zy + "px"
     };
 
@@ -614,12 +615,12 @@ function create_img(container, item, tl, zoom) {
 
     const [x, y, dx, dy] = item[1];
     const [zx, zy] = [zoom.x, zoom.y];
-    const style = { 
+    const style = {
         position: "absolute",
         overflow: "hidden",
         top: zy * (y - tl.y) + "px",
         left: zx * (x - tl.x) + "px",
-        width: dx * zx + "px", 
+        width: dx * zx + "px",
         height: dy * zy + "px"
     };
 
@@ -779,7 +780,7 @@ function create_asec(box, tl, z, type, style) {
 
     if (is_style_property(style.id))
         element.id = style.id;
-    
+
     return create_svg_element("path", element);
 }
 
@@ -894,9 +895,9 @@ function create_arc(p1, p2, large, tl, z, type="") {
 function create_circle(center, radius, tl, zx, zy, type="") {
     // Could be merged with create_ellipse()
     const [cx, cy] = center;
-    if (view.drawer.type === "rect") 
+    if (view.drawer.type === "rect")
         var [x, y] = [zx * (cx - tl.x), zy * (cy - tl.y)]
-    else 
+    else
         var {x, y} = cartesian_shifted(cx, cy, tl, zx);
 
     return create_svg_element("circle", {
@@ -908,9 +909,9 @@ function create_circle(center, radius, tl, zx, zy, type="") {
 
 function create_ellipse(center, rx, ry, tl, zx, zy, type="") {
     const [cx, cy] = center;
-    if (view.drawer.type === "rect") 
+    if (view.drawer.type === "rect")
         var [x, y] = [zx * (cx - tl.x), zy * (cy - tl.y)]
-    else 
+    else
         var {x, y} = cartesian_shifted(cx, cy, tl, zx);
 
     return create_svg_element("ellipse", {
@@ -921,7 +922,7 @@ function create_ellipse(center, rx, ry, tl, zx, zy, type="") {
 
 
 function create_slice(center, r, a, da, tl, zx, zy, type="", style) {
-    
+
     // Calculate center to translate slice
     const c = view.drawer.type === "rect"
         ? { x: zx * (center[0] - tl.x), y: zy * (center[1] - tl.y) }
@@ -943,7 +944,7 @@ function create_slice(center, r, a, da, tl, zx, zy, type="", style) {
 
     if (is_style_property(style.id))
         element.id = style.id;
-    
+
     return create_svg_element("path", element);
 }
 
@@ -979,8 +980,8 @@ function create_triangle(box, tip, tl, zx, zy, type="") {
 
     if (tip === "top")
         points.push(
-            [x + dx/2, y], 
-            [x, y + dy], 
+            [x + dx/2, y],
+            [x, y + dy],
             [x + dx, y + dy]);
     if (tip === "right")
         points.push(
@@ -990,15 +991,15 @@ function create_triangle(box, tip, tl, zx, zy, type="") {
     if (tip === "bottom")
         points.push(
             [x, y],
-            [x + dx/2, y + dy], 
+            [x + dx/2, y + dy],
             [x + dx, y]);
     if (tip === "left")
         points.push(
-            [x, y + dy/2], 
+            [x, y + dy/2],
             [x + dx, y + dy],
             [x + dx, y]);
 
-    if (view.drawer.type === "circ") 
+    if (view.drawer.type === "circ")
         points.forEach((point, idx, arr) => {
             const [r, a] = point;
             const {x: px, y: py} = cartesian_shifted(r, a, tl, zx);
@@ -1016,7 +1017,7 @@ function create_text(box, rotation, text, fs, tl, zx, zy, type="", anchor, style
 
     const element = {
         "class": "text " + type,
-        "text-anchor": (style && style.text_anchor 
+        "text-anchor": (style && style.text_anchor
                         ? style.text_anchor : "left"),
         "font-size": `${fs}px`,
     }
@@ -1150,7 +1151,7 @@ function is_style_property(property) {
 
 function style_line(line, style) {
     if (is_style_property(style.type))
-        line.style["stroke-dasharray"] = style.type === "solid" 
+        line.style["stroke-dasharray"] = style.type === "solid"
             ? null : style.type == "dotted"
             ? 2 : 5;
 
