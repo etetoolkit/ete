75008cc3bec2796be3a250cc06d5ff220c39d67d
Jordi <jordi.burguet.castell@gmail.com>
2023-03-28 12:51:04 +0200

    Remove example evol data from the repository (moved to ete-data).
    We had quite a few data files, and even duplicated, which are better
    in a separate repository. So we have moved them to another repository:
    https://github.com/etetoolkit/ete-data/tree/main/examples/evol_data
    
    Also, there are some comments in examples/evol/README, and some updates in the
    test at ete4/test/test_ete_evol.py (but the test does not work, before or after
    the changes -- it just is closer to being fixed).

--- ete4/test/test_ete_evol.py
+++ ete4/test/test_ete_evol.py
@@ -1,9 +1,7 @@
-from __future__ import absolute_import
-from __future__ import print_function
-
 import os
 import multiprocessing
 CPUS = min(20, max(1, multiprocessing.cpu_count()))
+
 import unittest
 
 from ..tools import ete
@@ -12,34 +10,43 @@ from ..evol.control import AVAIL
 BASEPATH = os.path.abspath(os.path.split(os.path.realpath(__file__))[0])
 DATAPATH = os.path.join(BASEPATH, "ete_evol_data", "S_example")
 SDATAPATH = os.path.join(BASEPATH,"ete_evol_data", "XS_example")
-OUTPATH = 'ete_test_tmp/ete3_evol-test/'
+OUTPATH = 'ete_test_tmp/ete4_evol-test/'
+
 
 class Test_ete_evol(unittest.TestCase):
     def test_01_all_models(self):
-        models = [k for k in AVAIL.keys() if not k.startswith('XX.')]
+        cmd = (f'ete4 evol'
+               f'    --mark Pan_troglodytes --noimg'
+               f'    --alg {SDATAPATH}/ali.fasta -t {SDATAPATH}/tree.nw'
+               f'    --clear_all -o {OUTPATH} --cpu {CPUS}'
+               f'    --model ') + ' '.join(model for model in AVAIL if not 'XX' in model))
 
-        cmd = 'ete3 evol --mark Pan_troglodytes --noimg --alg %s/ali.fasta -t %s/tree.nw --clear_all -o %s --cpu %d --model ' %(
-            SDATAPATH, SDATAPATH, OUTPATH, CPUS)
+        print(cmd)
         args = cmd.split()
-        args.extend([model for model in models if not 'XX' in model])
-        print(' '.join(args))
         ete._main(args)
 
     def test_02_web_examples(self):
-        commands = [
-            'ete3 evol -t ECP_EDN_15.nw --alg ECP_EDN_15.fasta -o ete_test_tmp/results1/ --models fb M2 SLR --cpu $$CPU',
-            'ete3 evol -t ECP_EDN_15.nw --alg ECP_EDN_15.fasta --models b_neut b_free --mark Papio_EDN,,,Orang_EDN -o ete_test_tmp/results2/ --cpu $$CPU',
-            'ete3 evol -t ECP_EDN_15.nw --alg ECP_EDN_15.fasta --models b_neut b_free --mark Human_EDN,,,Hylobates_EDN,Macaq_EDN,,,Papio_EDN Macaq_ECP,,Macaq2_ECP,Human_ECP,,Goril_ECP -o ete_test_tmp/results3/ --cpu $$CPU',
-            'ete3 evol -t ECP_EDN_15.nw --alg ECP_EDN_15.fasta --models b_neut b_free --mark Papio_EDN,,,Orang_EDN -o ete_test_tmp/results3/ --cpu $$CPU',
-            'ete3 evol -t ECP_EDN_15.nw --alg ECP_EDN_15.fasta --models M2 M1 b_free b_neut --leaves --tests b_free,b_neut -o ete_test_tmp/results4/ --cpu $$CPU',
-            ]
-        for cmd in commands:
-            cmd = cmd.replace("ECP_EDN_15", "%s/ete_evol_data/CladeModelCD/ECP_EDN_15" %BASEPATH)
-            cmd = cmd.replace("$$CPU", str(CPUS))
+        # TODO: Download the data from the ete-data repository to a
+        # temporary directory (with tempfile module?).
+
+        dpath = BASEPATH + '/ete_evol_data/CladeModelCD'
+        prefix = f'ete4 evol -t {dpath}/ECP_EDN_15.nw --alg {dpath}/ECP_EDN_15.fasta --cpu {CPUS} '
+
+        params = [
+            '--models fb M2 SLR -o ete_test_tmp/results1/',
+            '--models b_neut b_free --mark Papio_EDN,,,Orang_EDN -o ete_test_tmp/results2/',
+            '--models b_neut b_free --mark Human_EDN,,,Hylobates_EDN,Macaq_EDN,,,Papio_EDN Macaq_ECP,,Macaq2_ECP,Human_ECP,,Goril_ECP -o ete_test_tmp/results3/',
+            '--models b_neut b_free --mark Papio_EDN,,,Orang_EDN -o ete_test_tmp/results3/',
+            '--models M2 M1 b_free b_neut --leaves --tests b_free,b_neut -o ete_test_tmp/results4/']
+
+        for param in params:
+            cmd = prefix + param
             print(cmd)
             args = cmd.split()
             ete._main(args)
+
         os.system('rm -rf ete_test_tmp')
-        
+
+
 if __name__ == "__main__":
     unittest.main()
--- examples/evol/data/protamine/PRM1/slr/slr.ctl
+++
@@ -1,4 +0,0 @@
-seqfile: algn
-treefile: tree.slr
-outfile: eutherians_prm1_slr.txt
-positive_only: 0
