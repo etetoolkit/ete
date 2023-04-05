b910d21a6171f3ac61cf01dd43d298bcc47b5f41
Jordi <jordi.burguet.castell@gmail.com>
2023-04-06 00:24:04 +0200

    phyloxml_parser.py: ete3 -> ete4 (and python3)

--- examples/phyloxml/phyloxml_parser.py
+++ examples/phyloxml/phyloxml_parser.py
@@ -1,16 +1,16 @@
-from ete3 import Phyloxml
+from ete4 import Phyloxml
 project = Phyloxml()
 project.build_from_file("apaf.xml")
 
 # Each tree contains the same methods as a PhyloTree object
 for tree in project.get_phylogeny():
-    print tree
+    print(tree)
     # you can even use rendering options
     tree.show()
     # PhyloXML features are stored in the phyloxml_clade attribute
     for node in tree:
-        print "Node name:", node.name
+        print("Node name:", node.name)
         for seq in node.phyloxml_clade.get_sequence():
             for domain in seq.domain_architecture.get_domain():
                 domain_data = [domain.valueOf_, domain.get_from(), domain.get_to()]
-                print "  Domain:", '\t'.join(map(str, domain_data))
+                print("  Domain:", '\t'.join(map(str, domain_data)))
