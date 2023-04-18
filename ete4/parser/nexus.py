b476f01d21957f63d8071cf9170083a948ca9f09
Jordi <jordi.burguet.castell@gmail.com>
2023-04-18 22:27:14 +0200

    Add nexus parser and tests.

---
+++ ete4/parser/nexus.py
@@ -0,0 +1,95 @@
+"""
+Read trees from a file in nexus format.
+"""
+
+# See https://en.wikipedia.org/wiki/Nexus_file
+
+import re
+
+from .newick import read_newick, write_newick
+
+
+class NexusError(Exception):
+    pass
+
+
+
+def load(fp, format=1):
+    return loads(fp.read(), format=format)
+
+
+def loads(text, format=1):
+    return {name: read_newick(newick, format=format)
+            for name,newick in get_trees(text).items()}
+
+
+def get_trees(text):
+    "Return the trees as {name: newick} with all the name transformations done"
+    if not re.match(r'^#NEXUS\s*\n', text, flags=re.I):
+        raise NexusError('text does not start with "#NEXUS"')
+
+    commands = get_section(text, 'TREES')
+
+    translate = {}
+    if 'TRANSLATE' in commands:
+        if len(commands['TRANSLATE']) != 1:
+            raise NexusError('multiple TRANSLATE commands')
+        pairs = commands['TRANSLATE'][0].split(',')
+        translate.update(pair.split(maxsplit=1) for pair in pairs)
+
+    trees = {}
+    for command in commands.get('TREE', []):
+        name_ugly, newick_ugly = command.split('=', maxsplit=1)
+
+        name = name_ugly.strip('\t\r\n "\'')
+        newick = newick_ugly.strip() + ';'
+
+        if newick.startswith('['):  # remove possible [&U] or comment
+            newick = newick[newick.find(']')+1:].strip()
+
+        trees[name] = apply_translations(translate, newick)
+
+    return trees
+
+
+def apply_translations(translate, newick):
+    "Return newick with the node names translated according to the given dict"
+    if not translate:
+        return newick
+
+    t = read_newick(newick, format=1)
+
+    for node in t:
+        if node.name in translate:
+            node.name = translate[node.name]
+
+    return write_newick(t, properties=[], format=1)
+
+
+def get_section(text, section_name):
+    "Return the commands ({name: [args]}) that correspond to the given section"
+    return get_sections(text).get(section_name.upper(), {})
+
+
+def get_sections(text):
+    "Return {section: commands} read from the full text of a nexus file"
+    pattern = r'\nBEGIN\s+(\w+)\s*;(.*?)\nEND\s*;'
+
+    sections = {}
+    for m in re.finditer(pattern, text, flags=re.I | re.S):
+        name, text_section = m.groups()
+        sections[name.upper()] = get_commands(text_section)
+
+    return sections
+
+
+def get_commands(text_section):
+    "Return a dict that for each command has a list with its arguments"
+    pattern = r';.*?(?=;)'
+
+    commands = {}
+    for m in re.finditer(pattern, ';' + text_section, flags=re.I | re.S):
+        name, args = m.group().strip(';\r\n\t ').split(maxsplit=1)
+        commands.setdefault(name.upper(), []).append(args)
+
+    return commands
---
+++ tests/test_nexus.py
@@ -0,0 +1,104 @@
+"""
+Tests related to the nexus module.
+"""
+
+# Here you can find more information on the nexus format and examples:
+#   https://en.wikipedia.org/wiki/Nexus_file
+#   http://wiki.christophchamp.com/index.php?title=NEXUS_file_format
+#   http://hydrodictyon.eeb.uconn.edu/eebedia/index.php/Phylogenetics:_NEXUS_Format
+
+from tempfile import TemporaryFile
+import unittest
+
+from ete4 import Tree
+from ete4.parser import nexus
+
+
+class TestNexus(unittest.TestCase):
+
+    def test_loads(self):
+        with TemporaryFile(mode='w+t') as fp:
+            fp.write("""#NEXUS
+BEGIN TAXA;
+    TaxLabels Scarabaeus Drosophila Aranaeus;
+END;
+
+BEGIN TREES;
+    Translate beetle Scarabaeus, fly Drosophila, spider Aranaeus;
+    Tree tree1 = ((1,2),3);
+    Tree tree2 = ((beetle,fly),spider);
+    Tree tree3 = ((Scarabaeus,Drosophila),Aranaeus);
+END;
+            """)
+            fp.seek(0)
+
+            trees = nexus.load(fp)
+
+            newicks = {name: t.write() for name, t in trees.items()}
+
+            assert newicks == {
+                'tree1': '((1:1,2:1)1:1,3:1);',
+                'tree2': '((Scarabaeus:1,Drosophila:1)1:1,Aranaeus:1);',
+                'tree3': '((Scarabaeus:1,Drosophila:1)1:1,Aranaeus:1);'}
+
+
+    def test_loads_bad_file(self):
+        def read_bad_file():
+            with TemporaryFile(mode='w+t') as fp:
+                fp.write("""not a nexus file
+
+                anything.""")
+                fp.seek(0)
+
+                nexus.load(fp)
+
+            self.assertRaises(nexus.NexusError, read_bad_file)
+
+
+    def test_get_trees(self):
+        text = """#nexus
+begin trees;
+  translate
+    1 Ephedra,
+    2 Gnetum,
+    3 Welwitschia,
+    4 Ginkgo,
+    5 Pinus
+  ;
+  tree one = [&U] (1,2,(3,(4,5)));
+  tree two = [&U] (1,3,(5,(2,4)));
+end;
+    """
+
+        trees = nexus.get_trees(text)
+
+        assert trees == {
+            'one': '(Ephedra:1,Gnetum:1,(Welwitschia:1,(Ginkgo:1,Pinus:1):1):1):0;',
+            'two': '(Ephedra:1,Welwitschia:1,(Pinus:1,(Gnetum:1,Ginkgo:1):1):1):0;'}
+
+    def test_loads_without_trees(self):
+        with TemporaryFile(mode='w+t') as fp:
+            fp.write("""#NEXUS
+    BEGIN TAXA;
+        TaxLabels Scarabaeus Drosophila Aranaeus;
+    END;
+
+    BEGIN TREES;
+    END;
+            """)
+            fp.seek(0)
+
+            trees = nexus.load(fp)
+            assert trees == {}
+
+
+        with TemporaryFile(mode='w+t') as fp:
+            fp.write("""#NEXUS
+    BEGIN TAXA;
+        TaxLabels Scarabaeus Drosophila Aranaeus;
+    END;
+            """)
+            fp.seek(0)
+
+            trees = nexus.load(fp)
+            assert trees == {}
