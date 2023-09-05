2d6b67dc26b64682d7ef41b0edf5b5bd1febe346
Jordi <jordi.burguet.castell@gmail.com>
2023-09-05 13:13:20 +0200

    Minor improving in text search.

--- ete4/smartview/gui/server.py
+++ ete4/smartview/gui/server.py
@@ -1154,9 +1154,9 @@ def get_search_function(text):
     if text.startswith('/'):
         return get_command_search(text)  # command-based search
     elif text == text.lower():  # case-insensitive search
-        return lambda node: node.name and text in node.name.lower()
+        return lambda node: text in node.props.get('name', '').lower()
     else:  # case-sensitive search
-        return lambda node: node.name and text in node.name
+        return lambda node: text in node.props.get('name', '')
 
 
 def get_command_search(text):
@@ -1169,7 +1169,7 @@ def get_command_search(text):
 
     command, arg = parts
     if command == '/r':  # regex search
-        return lambda node: node.name and re.search(arg, node.name)
+        return lambda node: re.search(arg, node.props.get('name', ''))
     elif command == '/e':  # eval expression
         return get_eval_search(arg)
     elif command == '/t':  # topological search
