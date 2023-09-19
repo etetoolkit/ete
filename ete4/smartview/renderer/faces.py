7a4915d8b0e1078a7b3e5c50129ccf9a3683a3eb
Jordi <jordi.burguet.castell@gmail.com>
2023-09-19 11:57:54 +0200

    faces.py: Simplify and cleanup StackedBarFace.

--- ete4/smartview/renderer/faces.py
+++ ete4/smartview/renderer/faces.py
@@ -1696,55 +1696,35 @@ class LegendFace(Face):
                     style=text_style)
             ty += entry_h
 
-class StackedBarFace(RectFace):
-    def __init__(self, width, height, data=None, name="", opacity=0.7, 
-                min_fsize=6, max_fsize=15, ftype='sans-serif',
-                padding_x=0, padding_y=0, tooltip=None):
-        
-        RectFace.__init__(self, width=width, height=height, name=name, color=None, 
-            min_fsize=min_fsize, max_fsize=max_fsize, 
-            padding_x=padding_x, padding_y=padding_y, tooltip=tooltip)
-        
-        self.width = width
-        self.height = height
 
-        # data = [ [name, value, color, tooltip], ... ]
-        # self.data = [
-        #     ['first', 10, 'red', None],
-        #     ['second', 40, 'blue', None],
-        #     ['green', 50, 'green', None]
-        #     ]
+class StackedBarFace(RectFace):
+    """Face to show a series of stacked bars."""
+
+    def __init__(self, width, height, data=None, name='', opacity=0.7,
+                 min_fsize=6, max_fsize=15, ftype='sans-serif',
+                 padding_x=0, padding_y=0, tooltip=None):
+        """Initialize the face.
+
+        :param data: List of triples, like [(whatever, value, color), ...].
+        """
+        super().__init__(width=width, height=height, name=name, color=None,
+                         min_fsize=min_fsize, max_fsize=max_fsize,
+                         padding_x=padding_x, padding_y=padding_y, tooltip=tooltip)
         self.data = data
-        
 
     def __name__(self):
         return "StackedBarFace"
 
     def draw(self, drawer):
+        x0, y0, dx0, dy0 = self._box
 
-        # Draw RectFace if only one datum
+        total = sum(d[1] for d in self.data)
+        scale_factor = dx0 / (total or 1)  # the "or 1" prevents dividing by 0
 
-        if len(self.data) == 1:
-            self.color = self.data[0][2]
-            yield from RectFace.draw(self, drawer)
-
-        else:
-           
-            total_value = sum(d[1] for d in self.data)
-            start_x, start_y, dx, dy = self._box        
-            
-            for i in range(len(self.data)):
-                i_value = self.data[i][1]
-                color = self.data[i][2]
-                
-                if i > 0:
-                    start_x += new_dx # start with where last segment ends
-                
-                new_dx = i_value/total_value * dx # width of segment
-                
-                self._box = Box(start_x,start_y,new_dx,dy)
-                style = { 'fill': color }
-                yield draw_rect(self._box,
-                self.name,
-                style=style,
-                tooltip=self.tooltip)
+        x = x0
+        for _, value, color in self.data:
+            dx = scale_factor * value
+            box = Box(x, y0, dx, dy0)
+            yield draw_rect(box, self.name, style={'fill': color},
+                            tooltip=self.tooltip)
+            x += dx
