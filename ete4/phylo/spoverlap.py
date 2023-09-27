7717a181b707b4e855f4652e4dc5985631e3f3ba
Jordi <jordi.burguet.castell@gmail.com>
2023-09-27 19:19:25 +0200

    spoverlap.py: Fix undefined call to node distance in get_evol_events_from_leaf().
    The function called at a certain point:
      d += current.get_distance(current, leaf)
    and that could give an error, trying to add a float to None.
    
    Also, that "branch control" has not been used in a long time and
    important parts were commented out.
    
    This commit removes all legacy unused parts of that "branch control",
    avoiding that error. Also, it simplifies a few things.

--- ete4/phylo/spoverlap.py
+++ ete4/phylo/spoverlap.py
@@ -19,21 +19,12 @@ def get_evol_events_from_leaf(node, sos_thr=0.0):
     # Get the tree's root
     root = node.root
 
-    # Checks that is actually rooted
-    outgroups = root.get_children()
-    if len(outgroups) != 2:
+    try:
+        outg1, outg2 = root.get_children()  # outgroups
+    except ValueError:
         raise TypeError("Tree is not rooted")
 
-    # Cautch the smaller outgroup (will be stored as the tree
-    # outgroup)
-    o1 = set(n.name for n in outgroups[0].leaves())
-    o2 = set(n.name for n in outgroups[1].leaves())
-
-    if len(o2)<len(o1):
-        smaller_outg = outgroups[1]
-    else:
-        smaller_outg = outgroups[0]
-
+    smaller_outg = outg1 if len(outg1) < len(outg2) else outg2
 
     # Prepare to browse tree from leaf to root
     all_events = []
@@ -43,18 +34,15 @@ def get_evol_events_from_leaf(node, sos_thr=0.0):
     browsed_spcs   = set([current.species])
     browsed_leaves = set([current])
     # get family Size
-    fSize =  len([n for n in root.leaves() if n.species == ref_spcs])
+    fSize = sum(1 for n in root.leaves() if n.species == ref_spcs)
 
     # Clean previous analysis
-    for n in list(root.descendants())+[root]:
+    for n in root.traverse():
         n.del_prop("evoltype")
 
     while current.up:
-        # distances control (0.0 distance check)
-        d = 0
         for s in current.get_sisters():
             for leaf in s.leaves():
-                d += current.get_distance(current, leaf)
                 sister_leaves.add(leaf)
         # Process sister node only if there is any new sequence.
         # (previene dupliaciones por nombres repetidos)
@@ -80,7 +68,7 @@ def get_evol_events_from_leaf(node, sos_thr=0.0):
         event.inparalogs  = set([n.name for n in browsed_leaves if n.species == ref_spcs])
 
         # If species overlap: duplication
-        if score > sos_thr:# and d > 0.0: Removed branch control.
+        if score > sos_thr:
             event.node = current.up
             event.etype = "D"
             event.outparalogs = set([n.name for n in sister_leaves  if n.species == ref_spcs])
