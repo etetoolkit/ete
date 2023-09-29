5b349458e980d40c9c27d03aac6ffe7a3ecc732d
Jordi <jordi.burguet.castell@gmail.com>
2023-09-29 20:13:18 +0200

    Remove old files coming from when ete managed the users and trees database.
    These files were used with the "minimalist" version, which was able to
    store and retrieve trees from a sqlite database, manage users and
    permissions, etc.
    
    Since that version was integrated with ete4, these files are not used
    and make no sense. So, bye.

--- ete4/smartview/gui/sample_data.sql
+++
@@ -1,9 +0,0 @@
--- Run this file to add sample values into the database.
-
-insert into trees values
-    (1, 'Sample Tree', 'A simple test of a tree', '2021-01-01 00:00:00.000000',
-    '(B:2,(C:3,D:4)E:5)A:1;'),
-    (2, 'Multinest', 'An empty-looking tree', '2021-01-01 00:00:00.000000',
-    '(((((((((,),),),),),),),),);'),
-    (3, 'Directories', 'From the filesystem', '2021-01-01 00:00:00.000000',
-    '(boot/,dev/,etc/,(user1/,user2/)home/,(bin/,lib/)usr/,var/,tmp/)/;');
