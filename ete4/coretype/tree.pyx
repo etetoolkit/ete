etetoolkit/ete

commit cc3aaccad7dce750b3c02db3869710beba140a49

tree.pyx: Fix t.prune() when preserving branch lengths and node does not have it.
And simplify it a little.

ete4/coretype/tree.pyx
@@ -617,10 +617,11 @@ cdef class Tree(object):
 
         for n in self.descendants('postorder'):
             if n not in to_keep:
-                if preserve_branch_length:
+                if preserve_branch_length and n.dist is not None:
                     if len(n.children) == 1:
-                        n.children[0].dist += n.dist
-                    elif len(n.children) > 1 and n.up and n.up.dist:
+                        if n.children[0].dist is not None:
+                            n.children[0].dist += n.dist
+                    elif n.up and n.up.dist is not None:
                         n.up.dist += n.dist
 
                 n.delete(prevent_nondicotomic=False)

commit 4f1cbb40c9b3ad7c5e91e4d175d2906694a20743

tree.pyx: Fix t.prune()
There was a bug that I introduced in commit cc3aaccad7dce750.

ete4/coretype/tree.pyx
@@ -621,7 +621,7 @@ cdef class Tree(object):
                     if len(n.children) == 1:
                         if n.children[0].dist is not None:
                             n.children[0].dist += n.dist
-                    elif n.up and n.up.dist is not None:
+                    elif len(n.children) > 1 and n.up and n.up.dist is not None:
                         n.up.dist += n.dist
 
                 n.delete(prevent_nondicotomic=False)