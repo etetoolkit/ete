ed1186f7e274359e084bc8610bb2219c5b3fecdc
Jordi <jordi.burguet.castell@gmail.com>
2023-10-24 18:21:54 +0200

    Add tutorial on drawing with treeview.
    It is still not finished, but at least it is mostly done.

--- examples/treeview/bubble_map.py
+++ examples/treeview/bubble_map.py
@@ -1,48 +1,53 @@
 import random
-from ete3 import Tree, TreeStyle, NodeStyle, faces, AttrFace, CircleFace
+
+from ete4 import Tree
+from ete4.treeview import TreeStyle, NodeStyle, faces, AttrFace, CircleFace
+
 
 def layout(node):
     if node.is_leaf:
-        # Add node name to laef nodes
-        N = AttrFace("name", fsize=14, fgcolor="black")
+        # Add node name to leaf nodes.
+        N = AttrFace('name', fsize=14, fgcolor='black')
         faces.add_face_to_node(N, node, 0)
-    if "weight" in node.features:
-        # Creates a sphere face whose size is proportional to node's
-        # feature "weight"
-        C = CircleFace(radius=node.weight, color="RoyalBlue", style="sphere")
-        # Let's make the sphere transparent
+
+    if 'weight' in node.props:
+        # Create a sphere face whose size is proportional to the node's  'weight'.
+        C = CircleFace(radius=node.props['weight'], color='RoyalBlue', style='sphere')
+        # Let's make the sphere transparent.
         C.opacity = 0.3
-        # And place as a float face over the tree
-        faces.add_face_to_node(C, node, 0, position="float")
+        # And place it as a float face over the tree.
+        faces.add_face_to_node(C, node, 0, position='float')
+
 
 def get_example_tree():
-    # Random tree
+    # Make a random tree.
     t = Tree()
     t.populate(20, random_branches=True)
 
-    # Some random features in all nodes
+    # Add some random weight to all nodes.
     for n in t.traverse():
         n.add_props(weight=random.randint(0, 50))
 
-    # Create an empty TreeStyle
+    # Create an empty TreeStyle.
     ts = TreeStyle()
 
-    # Set our custom layout function
+    # Set our custom layout function.
     ts.layout_fn = layout
 
-    # Draw a tree
-    ts.mode = "c"
+    # Draw a circular tree.
+    ts.mode = 'c'
 
-    # We will add node names manually
+    # We will add node names manually.
     ts.show_leaf_name = False
-    # Show branch data
+    # Show branch data.
     ts.show_branch_length = True
     ts.show_branch_support = True
 
     return t, ts
 
-if __name__ == "__main__":
-    t, ts = get_example_tree()
 
-    #t.render("bubble_map.png", w=600, dpi=300, tree_style=ts)
+if __name__ == '__main__':
+    t, ts = get_example_tree()
     t.show(tree_style=ts)
+    # If we want to save it, we can do:
+    #   t.render('bubble_map.png', w=600, dpi=300, tree_style=ts)
--- examples/treeview/img_faces/img_faces.py
+++ examples/treeview/img_faces/img_faces.py
@@ -1,7 +1,8 @@
-# Import Tree instance and faces module
-from ete3 import Tree, faces, TreeStyle
+# Import Tree instance and faces module.
+from ete4 import Tree
+from ete4.treeview import faces, TreeStyle
 
-# Loads an example tree
+# Load an example tree.
 nw = """
 (((Dre:0.008339,Dme:0.300613)1.000000:0.596401,
 (Cfa:0.640858,Hsa:0.753230)1.000000:0.182035)1.000000:0.106234,
@@ -11,124 +12,118 @@ nw = """
 t = Tree(nw)
 
 # You can create any random tree containing the same leaf names, and
-# layout will work equally
-#
-# t = Tree()
-# Creates a random tree with 8 leaves using a given set of names
-# t.populate(8, ["Dme", "Dre", "Hsa", "Ptr", "Cfa", "Mms"])
+# layout will work equally. For example, creating a random tree with 6
+# leaves using a given set of names:
+#   t = Tree()
+#   t.populate(6, ['Dme', 'Dre', 'Hsa', 'Ptr', 'Cfa', 'Mms'])
 
-# Set the path in which images are located
-img_path = "./"
-# Create faces based on external images
-humanFace = faces.ImgFace(img_path+"human.png")
-mouseFace = faces.ImgFace(img_path+"mouse.png")
-dogFace = faces.ImgFace(img_path+"dog.png")
-chimpFace = faces.ImgFace(img_path+"chimp.png")
-fishFace = faces.ImgFace(img_path+"fish.png")
-flyFace = faces.ImgFace(img_path+"fly.png")
+# Set the path where images are located.
+img_path = './'
 
-# Create a faces ready to read the name attribute of nodes
-#nameFace = faces.TextFace(open("text").readline().strip(), fsize=20, fgcolor="#009000")
-nameFace = faces.AttrFace("name", fsize=20, fgcolor="#009000")
+# Create faces based on external images.
+humanFace = faces.ImgFace(img_path+'human.png')
+mouseFace = faces.ImgFace(img_path+'mouse.png')
+dogFace = faces.ImgFace(img_path+'dog.png')
+chimpFace = faces.ImgFace(img_path+'chimp.png')
+fishFace = faces.ImgFace(img_path+'fish.png')
+flyFace = faces.ImgFace(img_path+'fly.png')
 
-# Create a conversion between leaf names and real names
+# Create a face ready to read the name attribute of nodes.
+nameFace = faces.AttrFace('name', fsize=20, fgcolor='#009000')
+
+# Create a conversion between leaf names and real names.
 code2name = {
-        "Dre":"Drosophila melanogaster",
-        "Dme":"Danio rerio",
-        "Hsa":"Homo sapiens",
-        "Ptr":"Pan troglodytes",
-        "Mms":"Mus musculus",
-        "Cfa":"Canis familiaris"
-        }
+    'Dre': 'Drosophila melanogaster',
+    'Dme': 'Danio rerio',
+    'Hsa': 'Homo sapiens',
+    'Ptr': 'Pan troglodytes',
+    'Mms': 'Mus musculus',
+    'Cfa': 'Canis familiaris'}
 
-# Creates a dictionary with the descriptions of each leaf name
+# Create a dictionary with the descriptions of each leaf name.
 code2desc = {
-        "Dre":"""The zebrafish, also known as Danio rerio,
+    'Dre': """The zebrafish, also known as Danio rerio,
 is a tropical freshwater fish belonging to the
 minnow family (Cyprinidae).""",
-        "Dme":"""True flies are insects of the order Diptera,
+    'Dme': """True flies are insects of the order Diptera,
 possessing a single pair of wings on the
 mesothorax and a pair of halteres, derived from
 the hind wings, on the metathorax""",
-        "Hsa":"""A human is a member of a species
+    'Hsa': """A human is a member of a species
 of bipedal primates in the family Hominidae.""",
-        "Ptr":"""Chimpanzee, sometimes colloquially
+    'Ptr': """Chimpanzee, sometimes colloquially
 chimp, is the common name for the
 two extant species of ape in the genus Pan.""",
-        "Mms":"""A mouse is a small mammal belonging to the
+    'Mms': """A mouse is a small mammal belonging to the
 order of rodents.""",
-        "Cfa": """The dog (Canis lupus familiaris) is a
+    'Cfa': """The dog (Canis lupus familiaris) is a
 domesticated subspecies of the Gray Wolf,
 a member of the Canidae family of the
-orderCarnivora."""
-        }
+orderCarnivora."""}
 
-# Creates my own layout function. I will use all previously created
-# faces and will set different node styles depending on the type of
-# node.
+# Create a layout function. We will use all previously created faces
+# and will set different node styles depending on the type of node.
 def mylayout(node):
-    # If node is a leaf, add the nodes name and a its scientific
-    # name
+    # If node is a leaf, add the node names and their scientific names.
     if node.is_leaf:
-        # Add an static face that handles the node name
+        # Add an static face that handles the node name.
         faces.add_face_to_node(nameFace, node, column=0)
-        # We can also create faces on the fly
+        # We can also create faces on the fly.
         longNameFace = faces.TextFace(code2name[node.name])
         faces.add_face_to_node(longNameFace, node, column=0)
 
-        # text faces support multiline. We add a text face
-        # with the whole description of each leaf.
+        # Text faces support multiline. We add a text face with the
+        # whole description of each leaf.
         descFace = faces.TextFace(code2desc[node.name], fsize=10)
         descFace.margin_top = 10
         descFace.margin_bottom = 10
         descFace.border.margin = 1
 
-        # Note that this faces is added in "aligned" mode
+        # Note that this faces are added in "aligned" mode.
         faces.add_face_to_node(descFace, node, column=0, aligned=True)
 
-        # Sets the style of leaf nodes
-        node.img_style["size"] = 12
-        node.img_style["shape"] = "circle"
-    #If node is an internal node
-    else:
-        # Sets the style of internal nodes
-        node.img_style["size"] = 6
-        node.img_style["shape"] = "circle"
-        node.img_style["fgcolor"] = "#000000"
+        # Set the style of leaf nodes.
+        node.img_style['size'] = 12
+        node.img_style['shape'] = 'circle'
+    else:  # for internal nodes
+        # Set the style of internal nodes.
+        node.img_style['size'] = 6
+        node.img_style['shape'] = 'circle'
+        node.img_style['fgcolor'] = '#000000'
 
-    # If an internal node contains more than 4 leaves, add the
-    # images of the represented species sorted in columns of 2
-    # images max.
+    # If an internal node contains more than 4 leaves, add the images
+    # of the represented species sorted in columns of 2 images max.
     if len(node)>=4:
         col = 0
-        for i, name in enumerate(set(node.get_leaf_names())):
-            if i>0 and i%2 == 0:
+        for i, name in enumerate(set(node.leaf_names())):
+            if i > 0 and i % 2 == 0:
                 col += 1
             # Add the corresponding face to the node
-            if name.startswith("Dme"):
+            if name.startswith('Dme'):
                 faces.add_face_to_node(flyFace, node, column=col)
-            elif name.startswith("Dre"):
+            elif name.startswith('Dre'):
                 faces.add_face_to_node(fishFace, node, column=col)
-            elif name.startswith("Mms"):
+            elif name.startswith('Mms'):
                 faces.add_face_to_node(mouseFace, node, column=col)
-            elif name.startswith("Ptr"):
+            elif name.startswith('Ptr'):
                 faces.add_face_to_node(chimpFace, node, column=col)
-            elif name.startswith("Hsa"):
+            elif name.startswith('Hsa'):
                 faces.add_face_to_node(humanFace, node, column=col)
-            elif name.startswith("Cfa"):
+            elif name.startswith('Cfa'):
                 faces.add_face_to_node(dogFace, node, column=col)
 
             # Modifies this node's style
-            node.img_style["size"] = 16
-            node.img_style["shape"] = "sphere"
-            node.img_style["fgcolor"] = "#AA0000"
+            node.img_style['size'] = 16
+            node.img_style['shape'] = 'sphere'
+            node.img_style['fgcolor'] = '#AA0000'
 
-    # If leaf is "Hsa" (homo sapiens), highlight it using a
-    # different background.
-    if node.is_leaf and node.name.startswith("Hsa"):
-        node.img_style["bgcolor"] = "#9db0cf"
+    # If leaf is 'Hsa' (homo sapiens), highlight it using a different
+    # background.
+    if node.is_leaf and node.name.startswith('Hsa'):
+        node.img_style['bgcolor'] = '#9db0cf'
 
-# And, finally, Visualize the tree using my own layout function
+# And, finally, visualize the tree using our own layout function.
 ts = TreeStyle()
 ts.layout_fn = mylayout
-t.render("img_faces.png", w=600, tree_style = ts)
+
+t.render('img_faces.png', w=600, tree_style=ts)
