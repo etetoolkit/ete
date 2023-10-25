71477476f2ab9aed409479510911ace68aadb5c2
Jordi <jordi.burguet.castell@gmail.com>
2023-10-25 10:45:42 +0200

    Import faces with the smartview submodule.

--- ete4/smartview/__init__.py
+++ ete4/smartview/__init__.py
@@ -1,5 +1,13 @@
-from .renderer.face_positions import FACE_POSITIONS, make_faces
 from .renderer.treestyle import TreeStyle
-from .renderer.treelayout import TreeLayout
 from .renderer.nodestyle import NodeStyle
+
+from .renderer.treelayout import TreeLayout
 from .renderer import layouts as layout_modules
+
+from .renderer.face_positions import FACE_POSITIONS, make_faces
+
+from .renderer.faces import (
+    Face, TextFace, AttrFace, CircleFace, RectFace, ArrowFace, SelectedFace,
+    SelectedCircleFace, SelectedRectFace, OutlineFace, AlignLinkFace, SeqFace,
+    SeqMotifFace, AlignmentFace, ScaleFace, PieChartFace, HTMLFace, ImgFace,
+    LegendFace, StackedBarFace)
