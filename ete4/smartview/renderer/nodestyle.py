82687c87326ee878434b9c87675c91bd4a3ce36d
dengzq1234 <dengziqi1234@gmail.com>
2023-11-03 16:05:20 +0100

    replace sphere with triangle in the checker

--- ete4/smartview/renderer/nodestyle.py
+++ ete4/smartview/renderer/nodestyle.py
@@ -4,7 +4,7 @@ _LINE_TYPE_CHECKER = lambda x: x in (0, 1, 2)
 _SIZE_CHECKER = lambda x: isinstance(x, int)
 _COLOR_MATCH = re.compile("^#[A-Fa-f\d]{6}$")
 _COLOR_CHECKER = lambda x: x.lower() in SVG_COLORS or re.match(_COLOR_MATCH, x)
-_NODE_TYPE_CHECKER = lambda x: x in ["sphere", "circle", "square"]
+_NODE_TYPE_CHECKER = lambda x: x in ["triangle", "circle", "square"]
 _BOOL_CHECKER = lambda x: isinstance(x, bool) or x in (0, 1)
 _FLOAT_CHECKER = lambda x: isinstance(x, float) or isinstance(x, int)
 
