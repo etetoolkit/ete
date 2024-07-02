beee4b44ec1f40e6def50658aa0310e54c81f6f3
Jordi <jordi.burguet.castell@gmail.com>
2024-07-02 20:21:56 +0200

    ete_explore.py: Fix it so it works with the new explore().
    And some cosmetic changes too.

--- ete4/tools/ete_explore.py
+++ ete4/tools/ete_explore.py
@@ -1,8 +1,9 @@
+import os
+
 from .common import src_tree_iterator
 
 from ete4 import PhyloTree
-from ete4.smartview import TreeStyle
-from ete4.smartview.gui.server import run_smartview
+from ete4.smartview.explorer import explore
 
 DESC = "Launches an instance of the ETE smartview tree explorer server."
 
@@ -13,21 +14,13 @@ def populate_args(explore_args_p):
               "'value:@dist, pos:b-top, color:red, size:10, if:@dist>0.9'"))
 
 
-
 def run(args):
-
-    # VISUALIZATION
-    # Basic tree style
-    ts = TreeStyle()
-    ts.show_leaf_name = True
     try:
         tfile = next(src_tree_iterator(args))
-    except StopIteration:
-        run_smartview()
-    else:
         t = PhyloTree(open(tfile), parser=args.src_newick_format)
-        t.explore(name=tfile)
-        try:
-            input('Running ete explorer. Press enter to finish the session.\n')
-        except KeyboardInterrupt:
-            pass  # it's okay if the user exits with Ctrl+C too
+        t.explore(name=os.path.basename(tfile))
+        input('Running ete explorer. Press enter to finish the session.\n')
+    except StopIteration:
+        explore()  # if there is no input file, we still open the explorer
+    except (KeyboardInterrupt, EOFError):
+        pass  # it's okay if the user exits with Ctrl+C too
