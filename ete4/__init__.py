df2081d20a21e8cfa5b40b8656d8a1d1bc810f8c
Jordi <jordi.burguet.castell@gmail.com>
2024-07-30 17:17:53 +0200

    cosmetics

--- ete4/__init__.py
+++ ete4/__init__.py
@@ -1,22 +1,26 @@
-# Note that the use of "from x import *" is safe here. Modules include
-# the __all__ variable.
-from warnings import warn
-
 from .core.tree import Tree, TreeError
+
 from .core import operations, text_viz
+
+from .core.arraytable import ArrayTable
+from .core.seqgroup import SeqGroup
+
 from .parser import newick, ete_format, nexus
+
 from .config import (ETE_DATA_HOME, ETE_CONFIG_HOME, ETE_CACHE_HOME,
                      update_ete_data)
+
 from .ncbi_taxonomy import *
 from .gtdb_taxonomy import *
-from .core.seqgroup import *
+
 from .phylo.phylotree import *
 from .evol.evoltree import *
+from .evol import EvolTree
+from .clustering.clustertree import *
+
 from .phyloxml import Phyloxml, PhyloxmlTree
 from .nexml import Nexml, NexmlTree
-from .evol import EvolTree
-from .core.arraytable import *
-from .clustering.clustertree import *
+
 from .utils import SVG_COLORS, COLOR_SCHEMES, random_color
 
 from .version import __version__
