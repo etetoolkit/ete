3f66a0d3899e3e9159b5c68be10e491a377a70e3
Jordi <jordi.burguet.castell@gmail.com>
2024-10-11 11:39:00 -0400

    menu.js: Use a better name for the folder that can modify the tree style.

--- ete4/smartview/static/js/menu.js
+++ ete4/smartview/static/js/menu.js
@@ -187,7 +187,7 @@ function add_folder_viewport(menu) {
 
 
 function add_folder_style(menu) {
-    const folder_style = folder(menu, "style");
+    const folder_style = folder(menu, "tree style");
 
     folder_style.addButton({title: "use original tree style"})
       .on("click", async () => {
