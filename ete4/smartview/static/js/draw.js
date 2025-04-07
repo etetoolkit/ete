13ba536cc16fbfaa67e2d3068b4b7294a48498e9
Jordi <jordi.burguet.castell@gmail.com>
2025-04-07 11:14:11 +0200

    Fix font size and used size for text that appears rotated inside boxes/rects/etc.
    The formulas are a bit tricky. I intend to document them in
    https://etetoolkit.github.io/ete/internals/index.html

--- ete4/smartview/static/js/draw.js
+++ ete4/smartview/static/js/draw.js
@@ -916,14 +916,15 @@ function create_polygon(center, r, shape, tl, zx, zy, style="", resize=false) {
 function create_text(box, anchor, text, fs_max, rotation,
                      tl, zx, zy, style="") {
     const [x, y, fs, text_anchor] = view.shape === "rectangular" ?
-        get_text_placement_rect(box, anchor, text, fs_max, tl, zx, zy, style) :
-        get_text_placement_circ(box, anchor, text, fs_max, tl, zx, style);
+        get_text_placement_rect(box, anchor, text, fs_max, rotation, tl, zx, zy, style) :
+        get_text_placement_circ(box, anchor, text, fs_max, rotation, tl, zx, style);
 
     const dx = (style === "name") ? view.name.padding.left * fs / 100 : 0;
 
     const t = create_svg_element("text", {
         "class": "text",
-        "x": x + dx, "y": y,
+        "x": x + dx,
+        "y": y,
         "font-size": `${fs}px`,
         "text-anchor": text_anchor,
     });
@@ -1189,16 +1190,20 @@ function pop_style(style, prop) {
 
 
 // Return position, font size and text anchor to draw text when box is a rect.
-function get_text_placement_rect(box, anchor, text, fs_max, tl, zx, zy, type="") {
+function get_text_placement_rect(box, anchor, text, fs_max, rotation,
+                                 tl, zx, zy, type="") {
     if (text.length === 0)
         throw new Error("please do not try to place empty texts :)")
         // We could, but it's almost surely a bug upstream!
 
     const [x, y, dx, dy] = box;
 
-    const dx_char = dx / text.length;  // ~ width of 1 char (in tree units)
-    fs_max = Math.min(zx * dx_char * 1.6, zy * dy, fs_max);
-    const fs = font_adjust(fs_max, type);
+    const a = rotation * Math.PI / 180;
+    const [c, s] = [Math.abs(Math.cos(a)), Math.abs(Math.sin(a))];
+
+    const w_h = text.length / 1.5;  // text width over its height
+    const fs_box = dx * zx / (s + w_h * c);  // font size so it fits in box
+    const fs = font_adjust(Math.min(fs_box, fs_max), type);
 
     const scale = fs / (zy * dy);
     const [ax, ay] = anchor;
@@ -1217,7 +1222,7 @@ function get_text_placement_rect(box, anchor, text, fs_max, tl, zx, zy, type="")
 
 
 // Return position, font size and text anchor to draw text when box is an asec.
-function get_text_placement_circ(box, anchor, text, fs_max, tl, z, type="") {
+function get_text_placement_circ(box, anchor, text, fs_max, rotation, tl, z, type="") {
     if (text.length === 0)
         throw new Error("please do not try to place empty texts :)");
         // We could, but it's almost surely a bug upstream!
