f1dd99ef1112d2b346f2b7d6b58696c5c9411d38
Jordi <jordi.burguet.castell@gmail.com>
2025-04-18 03:43:33 +0200

    faces.py: cosmetics
    Remove unused default anchor and comment a bit.

--- ete4/smartview/faces.py
+++ ete4/smartview/faces.py
@@ -27,13 +27,13 @@ from .coordinates import Size, Box, make_box
 from . import graphics as gr
 
 
+# Default anchors. The x, y go between -1 to +1, with 0 the center.
 default_anchors = {'top':     (-1, 1),   # left, bottom
                    'bottom':  (-1, -1),  # left, top
                    'right':   (-1, 0),   # left, middle
                    'left':    ( 1, 0),   # right, middle
                    'aligned': (-1, 0),   # left, middle
-                   'header':  (-1, 1),   # left, bottom
-                   'footer':  (-1, 1)}   # (unused for the moment)
+                   'header':  (-1, 1)}   # left, bottom
 
 
 class Face:
