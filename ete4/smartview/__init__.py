7dbc8ba9dc84436f0a9c40c0305d20d391cdf536
Jordi <jordi.burguet.castell@gmail.com>
2025-05-27 19:20:06 +0200

    Add more documentation on smartview and faces.

--- ete4/smartview/__init__.py
+++ ete4/smartview/__init__.py
@@ -1,3 +1,16 @@
+"""
+This module contains all the elements necessary to:
+
+- Launch a backend server to receive drawing (and other) requests and
+  send back the graphic commands and other information (``explorer``)
+- Define how to visualize trees (``layout``)
+- Use pieces of drawn information attached to the nodes (``faces``)
+
+The drawing logic exists in the ``draw`` submodule, the basic graphical
+elements that can be sent are defined in the ``graphics`` submodule,
+and the basic coordinate functions are defined in ``coordinates``.
+"""
+
 from .layout import Layout, BASIC_LAYOUT
 from .faces import (EvalTextFace, TextFace, TextArrayFace, PropFace,
                     CircleFace, PolygonFace, BoxFace, RectFace,
