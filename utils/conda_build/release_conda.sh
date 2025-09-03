ac24dea222d2af9f589f6ef3ff5ea66f52e2fedd
Jordi <jordi.burguet.castell@gmail.com>
2025-09-03 12:33:54 +0200

    Kind of update and add information on utils/conda_build

--- utils/conda_build/release_conda.sh
+++ utils/conda_build/release_conda.sh
@@ -1,5 +1,8 @@
-rm meta.yml build.sh 
+# TODO: See notes on meta.yaml.template.
+
+rm meta.yml build.sh
+
 git push &&
 python -c 'new = open("meta.yaml.template").read().replace("%VERSION%", open("../../VERSION").readline().strip()); open("meta.yaml", "w").write(new)' &&
 python -c 'new = open("build.sh.template").read().replace("%VERSION%", open("../../VERSION").readline().strip()); open("build.sh", "w").write(new)' &&
-conda build --python 3.7 ./
+conda build ./
